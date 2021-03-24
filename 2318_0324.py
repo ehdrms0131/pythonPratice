3538bec266c798f28f67347d5e6a9dac182ffad0
ehdrms0131 <ehdrms1182@gmail.com>
2021-03-25 08:26:13 +0900

    Update 2318_0324.py

--- 2318_0324.py
+++ 2318_0324.py
@@ -60,3 +60,40 @@ while i != 0 :
     print(i,"x",j,"=",i*j)
     j += 1
   i = int(input("단"))
+
+  n = 0
+while n != 1:
+  n = int(input("숫자입력"))
+  i = 1
+  s = 0
+  while i <= n:
+     s += i
+     i += 1
+  print(s)
+
+  p = 100
+while p > 0 :
+  d = int(input("얼마의 데미지를 입히시겠습니까"))  
+  p -= d
+  print("주인공의 체력은",p)
+  if p > 0 :
+    break
+print("주인공이 죽었습니다")
+
+l = ["아","점","저","야"]
+while a != 0 :
+  a = int(input("며칠이 지났습니까?"))
+  for i in l : 
+    print(a * i)
+
+    n = 0
+j = [10,30,61,70,90]
+print("2020 제2회 한국사 시험 결과")
+for i in j :
+  n += 1
+  if i >= 70 :
+    print(n,"1급")
+  elif i >= 60:
+    print(n,"2급")
+  else :
+    print(n,"불합격")
